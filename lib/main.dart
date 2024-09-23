586faf34a91615fc094636d4c392a7b731b2dbb4
emon22830 <mdemon22830@gmail.com>
2024-09-23 15:19:55 +0800

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey.shade700,
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: Text('StudentCampus'),
-      ),
-      body: const Center(
-        child: Image(
-            image: NetworkImage(
-                'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg')),
-      ),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey.shade700,
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey,
+            title: const Text('StudentCampus'),
+          ),
+          body: Center(
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue,
+              child: Text('Container'),
+              alignment: Alignment.center,
+            ),
+          )),
+    );
+  }
 }
