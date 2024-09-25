2055272e955a8f9902bfd3624282640a5b9e7e42
emon22830 <mdemon22830@gmail.com>
2024-09-25 11:03:29 +0800

    Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,22 +10,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.blueGrey.shade700,
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey,
-            title: const Text('StudentCampus'),
-          ),
-          body: Center(
-            child: Container(
-              height: 200,
-              width: 200,
-              color: Colors.blue,
-              child: Text('Container'),
-              alignment: Alignment.center,
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('About widget'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+
+              ],
             ),
-          )),
+            Center(
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50 ),
+                  height: 250,
+                  width: 250,
+
+
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.red,
+                      width: 1,
+                    ),
+                   image: DecorationImage(
+                     image: NetworkImage('https://cdn.photographylife.com/wp-content/uploads/2018/11/Moeraki-Boulders-New-Zealand.jpg'),
+                   fit: BoxFit.cover
+                   ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.red,
+                        blurRadius: 100,
+                      )
+                    ]
+                  ),
+
+                  child: const Center(child: Text(' Login ', style: TextStyle(fontSize: 20),),
+                  ),
+                ),
+              ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
