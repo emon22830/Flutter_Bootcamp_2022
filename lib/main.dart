8ee240687ff1fc9393a2bf392dfa3d4d44e6de0e
emon22830 <mdemon22830@gmail.com>
2024-10-02 12:14:45 +0800

    TextFormField

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,25 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
          crossAxisAlignment: CrossAxisAlignment.center,
-         children: [
-           Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: TextFormField(
-               keyboardType: TextInputType.phone,
-               cursorColor: Colors.red,
-               enabled: true,
-               style: TextStyle(fontSize: 14, color: Colors.green),
-               decoration: InputDecoration(
-                 prefixIcon: Icon(Icons.email),
-                 fillColor: Colors.green.withOpacity(.2),
-                 filled: true,
-                 hintText: 'Emal',
+        children: [
 
-                   hintStyle: TextStyle(
-                       fontSize: 14, color: Colors.red
-                   ),
-                 enabledBorder: OutlineInputBorder(
-                   borderSide: BorderSide(color: Colors.teal, width: 1),
-                 ),
-                 focusedBorder: OutlineInputBorder(
-                   borderSide: BorderSide(color: Colors.teal, width: 1,),
-                   borderRadius: BorderRadius.circular(10),
-                 ),
-               ),
-onChanged: (value){
-                 print(value);
-},
-             ),
-           ),
-         ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: 100,
+                itemBuilder: (context, index){
+                  return  const ListTile(
+                    leading: CircleAvatar(
+                      radius: 25,
+                      backgroundImage: NetworkImage('https://tse2.mm.bing.net/th?id=OIP.qQDgUP_imfPJZfqlrT_taAHaHa&pid=Api&P=0&h=220'),
+                    ),
+                    title: Text('MD EMON'),
+                    subtitle: Text('Software Engineer'),
+                    trailing: Text('3.52 PM'),
+                  );
+                }
+            ),
+          )
+        ],
 
         )
       ),
